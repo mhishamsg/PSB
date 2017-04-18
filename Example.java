7d9071ddc2a90106b8bb426d45941323f9748e4a
mhishamsg <mhishamsg@users.noreply.github.com>
2017-04-18 14:07:40 +0800

    Create Example.java
    first code created for java programming.

---
+++ Example.java
@@ -0,0 +1,9 @@
+/* This is a simple Java program.
+Call this file Example.java.
+*/
+class Example {
+	// A Java program begins with  call to main ().
+	public static void main(String args[]) {
+		System.out.println("Java powers 3 billion devices around the world.");
+}
+}
